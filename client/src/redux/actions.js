6748aa7aab584e62478d3230be7fab7055a0c6ec
nicobonder <nicobonder@hotmail.com>
2023-01-20 17:32:29 -0300

    deploying

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -19,7 +19,7 @@ export const SEARCH_POKEMON = "SEARCH_POKEMON";
 
 export const getPokemons = () => {
     return function(dispatch) {
-        return fetch('http://localhost:3001/pokemons')
+        return fetch('https://pokemonapi-jzai.onrender.com/pokemons')
         .then(res => res.json())
         .then(pokemons => dispatch(
             {type: GET_POKEMONS, payload: pokemons}
@@ -29,7 +29,7 @@ export const getPokemons = () => {
 
 export const getPokemonDetail = (id) => {
     return async function(dispatch) {
-        return fetch(`http://localhost:3001/pokemons/${id}`)    
+        return fetch(`https://pokemonapi-jzai.onrender.com/pokemons/${id}`)    
         .then(res => res.json())
         .then(data => dispatch(
             {type: GET_POKEMON_DETAIL, payload: data[0]}
@@ -40,7 +40,7 @@ export const getPokemonDetail = (id) => {
 export const searchPokemon = (name) => {
     return async function(dispatch) {
         try {
-            let info =  await axios.get("http://localhost:3001/pokemons?name=" + name);
+            let info =  await axios.get("https://pokemonapi-jzai.onrender.com/pokemons?name=" + name);
             return dispatch({
                 type: "SEARCH_POKEMON",
                 payload: info.data
@@ -53,14 +53,14 @@ export const searchPokemon = (name) => {
 
 export const createPokemon = (pokemon) => {
     return async function(dispatch){
-        const newPokemon = await axios.post(`http://localhost:3001/pokemons/`, pokemon)
+        const newPokemon = await axios.post(`https://pokemonapi-jzai.onrender.com/pokemons/`, pokemon)
        dispatch({type: CREATE_POKEMON, payload: newPokemon})
     }
 };
 
 export const updatePokemon = (updatePoke) => {
     return async function(dispatch){ //va la ruta del poke que tiene que actualizar
-        await axios.put(`http://localhost:3001/pokemons/${updatePoke.id}`, updatePoke)
+        await axios.put(`https://pokemonapi-jzai.onrender.com/pokemons/${updatePoke.id}`, updatePoke)
         .then((res) => { 
             dispatch({type: UPDATE_POKEMON, payload: res.data}) //res.data da OK
         })
@@ -72,14 +72,14 @@ export const updatePokemon = (updatePoke) => {
 
 export const deletePokemon = (pokemonId) => {
     return async function(dispatch){
-        const deletePoke = await axios.delete(`http://localhost:3001/pokemons/${pokemonId}`, pokemonId )
+        const deletePoke = await axios.delete(`https://pokemonapi-jzai.onrender.com/pokemons/${pokemonId}`, pokemonId )
         dispatch({type: DELETE_POKEMON, payload: deletePoke})
     }
 }
 
 export const getTypes = () => {
     return async function(dispatch){
-        let info = await axios.get('/types', {})
+        let info = await axios.get('https://pokemonapi-jzai.onrender.com/types', {})
         return dispatch({type: GET_TYPES, payload: info.data})
     }
 }
