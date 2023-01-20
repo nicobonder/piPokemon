6b899354fbd971fa794557070bac81b102626fb8
nicobonder <nicobonder@hotmail.com>
2023-01-20 17:54:01 -0300

    deploying again

--- client/src/index.js
+++ client/src/index.js
@@ -8,10 +8,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
-
-//import { Provider } from 'react-redux';
-//import store from './redux/store';
-
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+
 export const GET_POKEMONS = "GET_POKEMONS";
 export const GET_POKEMON_DETAIL = "GET_POKEMON_DETAIL";
 export const CREATE_POKEMON = "CREATE_POKEMON";
